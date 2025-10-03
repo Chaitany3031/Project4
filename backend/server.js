addd0b45d0d938510d1ed5f48b30d26fbf80f007
Chaitanya3031 <chaitanyamhatre2349@gmail.com>
2025-10-03 17:46:27 +0530

    socket io issue persist

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const app = require("./src/app");
 const connectToDb = require("./src/db/db");
-
+const initSocketServer = require('./src/sockets/socket.server');
+const httpServer = require('http').createServer(app);
 
 // Connect to the database
 connectToDb();
+
+// Pass httpServer instead of app
+initSocketServer(httpServer);
+
 const PORT = process.env.PORT || 3000;
-// KAVERI
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
